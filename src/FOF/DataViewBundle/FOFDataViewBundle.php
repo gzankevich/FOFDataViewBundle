ee63f0d2455fb04c22ca120285a64ee4cc804da8
George Zankevich <george.zankevich.fof@gmail.com>
2012-11-06 10:48:06 +0000

    [master] Initial commit

---
+++ src/FOF/DataViewBundle/FOFDataViewBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace FOF\DataViewBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class FOFDataViewBundle extends Bundle
+{
+
+}
