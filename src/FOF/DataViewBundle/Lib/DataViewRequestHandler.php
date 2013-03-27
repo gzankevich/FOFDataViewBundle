ecea57fe1e611b31adfd09f4a5aa43ad31aa4498
George Zankevich <george.zankevich.fof@gmail.com>
2013-03-27 16:25:08 +0000

    [I-710] Fixed sorting/filtering bug
    The current page was being applied to the fantapager causing the
    query to be built prematurely

--- src/FOF/DataViewBundle/Lib/DataViewRequestHandler.php
+++ src/FOF/DataViewBundle/Lib/DataViewRequestHandler.php
@@ -8,6 +8,11 @@ use DataView\Filter;
 use DataView\DataView;
 use FOF\DataViewBundle\Form\Type\DataViewType;
 
+/**
+ * The glue between a request and a DataView
+ *
+ * @author George Zankevich <gzankevich@gmail.com>
+ */
 class DataViewRequestHandler
 {
     const SESSION_KEY = 'data_view';
@@ -32,35 +37,52 @@ class DataViewRequestHandler
      */
     public function bind(DataView $dataView, Request $request)
     {
+        $this->handleFilters($dataView, $request);
+
         // initial page load
         if($request->getMethod() == 'GET') {
             $this->clearSessionSettings();
         } else {
-            $this->handleFilters($dataView, $request);
-
             // this will set the initial values on the DataView
             $this->loadSessionSettings($dataView);
 
             // these will override those values
             $this->handleSort($dataView, $request);
+
             $this->handlePagination($dataView, $request);
 
             $this->saveSessionSettings($dataView);
         }
     }
 
+    /**
+     * Clears the settings stored in the session
+     *
+     * @return null
+     */
     protected function clearSessionSettings()
     {
         $this->session->clear(self::SESSION_KEY);
     }
 
+    /**
+     * Loads settings from the session
+     *
+     * @param DataView $dataView The DataView instance to get the settings from
+     * @return null
+     */
     protected function loadSessionSettings($dataView)
     {
         $settings = $this->session->get(self::SESSION_KEY);
-
-        $dataView->getPager()->setCurrentPage(intval($settings['page']));
+        $dataView->setCurrentPage(intval($settings['page']));
     }
 
+    /**
+     * Saves settings to the session
+     *
+     * @param DataView $dataView The DataView instance to load the settings into
+     * @return null
+     */
     protected function saveSessionSettings($dataView)
     {
         $this->session->set(self::SESSION_KEY, array(
--- tests/DataViewRequestHandlerTest.php
+++ tests/DataViewRequestHandlerTest.php
@@ -4,13 +4,16 @@ namespace FOF\DataViewBundle\Test;
 
 use FOF\DataViewBundle\Lib\DataViewRequestHandler;
 
+/**
+ * Unit test for DataViewRequestHandler
+ *
+ * @author George Zankevich <gzankevich@gmail.com>
+ */
 class DataViewRequestHandlerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testFoo()
-    {
-        $this->assertTrue(true);
-    }
-
+    /**
+     * @covers DataViewRequestHandler::bind
+     */
     public function test_initialPageLoad()
     {
         $formFactory = $this->getMockBuilder('\Symfony\Component\Form\FormFactory')->disableOriginalConstructor()->getMock();
@@ -22,10 +25,14 @@ class DataViewRequestHandlerTest extends \PHPUnit_Framework_TestCase
             ->with($this->equalTo('data_view'));
 
         $dataView = $this->getMockBuilder('\DataView\DataView')->disableOriginalConstructor()->getMock();
+        $dataView
+            ->expects($this->once())
+            ->method('getColumns')
+            ->will($this->returnValue(array()));
 
         $request = $this->getMockBuilder('\Symfony\Component\HttpFoundation\Request')->disableOriginalConstructor()->getMock();
         $request
-            ->expects($this->once())
+            ->expects($this->exactly(2))
             ->method('getMethod')
             ->will($this->returnValue('GET'));
 
