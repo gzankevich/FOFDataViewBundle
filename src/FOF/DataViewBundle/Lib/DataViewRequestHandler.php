1bc1525d77e9bfe833745f402201dbf75e6a59b5
George Zankevich <george.zankevich.fof@gmail.com>
2013-06-04 15:49:52 +0100

    [master] Fixed pagination
    Better coverage for DataViewRequestHandler::bind

--- src/FOF/DataViewBundle/Lib/DataViewRequestHandler.php
+++ src/FOF/DataViewBundle/Lib/DataViewRequestHandler.php
@@ -3,6 +3,8 @@
 namespace FOF\DataViewBundle\Lib;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\FormFactory;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 use DataView\Filter;
 use DataView\DataView;
@@ -20,7 +22,11 @@ class DataViewRequestHandler
     protected $form, $formFactory, $session = null;
     protected $isBound = false;
 
-    public function __construct($formFactory, $session)
+    /**
+     * @param FormFactory $formFactory Used to instantiate the filter form
+     * @param Session $session Stores settings between pages
+     */
+    public function __construct(FormFactory $formFactory, Session $session)
     {
         $this->formFactory = $formFactory;
         $this->session     = $session;
@@ -71,7 +77,7 @@ class DataViewRequestHandler
      * @param DataView $dataView The DataView instance to get the settings from
      * @return null
      */
-    protected function loadSessionSettings($dataView)
+    protected function loadSessionSettings(DataView $dataView)
     {
         $settings = $this->session->get(self::SESSION_KEY);
         $dataView->setCurrentPage(intval($settings['page']));
@@ -83,49 +89,61 @@ class DataViewRequestHandler
      * @param DataView $dataView The DataView instance to load the settings into
      * @return null
      */
-    protected function saveSessionSettings($dataView)
+    protected function saveSessionSettings(DataView $dataView)
     {
         $this->session->set(self::SESSION_KEY, array(
-            'page' => $dataView->getPager()->getCurrentPage(), 
+            'page' => $dataView->getCurrentPage(), 
         ));
     }
 
-    protected function handlePagination(&$dataView, $request)
+    /**
+     * Modify the current page depending on what is in the request
+     *
+     * @param DataView $dataView The DataView instance to modify
+     * @param Request $request The Request to read input from
+     * @return null
+     */
+    protected function handlePagination(DataView $dataView, Request $request)
     {
         foreach($request->request->all() as $name => $order) {
             if($name == 'pagination_first_page') {
-                $dataView->getPager()->setCurrentPage(1);
+                $dataView->setCurrentPage(1);
             } elseif($name == 'pagination_previous_page') {
-                $dataView->getPager()->setCurrentPage($dataView->getPager()->getCurrentPage() - 1);
+                $dataView->setCurrentPage($dataView->getPager()->getCurrentPage() - 1);
             } elseif($name == 'pagination_next_page') {
-                $dataView->getPager()->setCurrentPage($dataView->getPager()->getCurrentPage() + 1);
+                $dataView->setCurrentPage($dataView->getPager()->getCurrentPage() + 1);
             } elseif($name == 'pagination_last_page') {
-                $dataView->getPager()->setCurrentPage($dataView->getPager()->getNbPages());
+                $dataView->setCurrentPage($dataView->getPager()->getNbPages());
             }
         }
 
     }
 
-    protected function handleSort($dataView, $request)
+    /**
+     * Apply sorting to the relevant column
+     *
+     * @param DataView $dataView The DataView instance to modify
+     * @param Request $request The Request to read input from
+     * @return null
+     */
+    protected function handleSort(DataView $dataView, Request $request)
     {
         foreach($request->request->all() as $name => $order) {
             if(strpos($name, 'sort_') === 0) {
-                $this->applySort($dataView, str_replace('__', '.', str_replace('sort_', '', $name)), $order);
+                $dataView->applySort(str_replace('__', '.', str_replace('sort_', '', $name)), $order);
                 return;
             }
         }
     }
 
-    protected function applySort($dataView, $propertyPath, $order)
-    {
-        foreach($dataView->getColumns() as $column) {
-            if($column->getPropertyPath() === $propertyPath) {
-                $column->setSortOrder($order);
-            }
-        }
-    }
-
-    protected function handleFilters($dataView, $request)
+    /**
+     * Applies filters
+     *
+     * @param DataView $dataView The DataView instance to modify
+     * @param Request $request The Request to read input from
+     * @return null
+     */
+    protected function handleFilters(DataView $dataView, Request $request)
     {
         $columns = array();
         foreach($dataView->getColumns() as $column) {
@@ -142,6 +160,11 @@ class DataViewRequestHandler
         }
     }
 
+    /**
+     * Get the filter form
+     *
+     * @return Form
+     */
     public function getForm()
     {
         return $this->form;
--- tests/DataViewRequestHandlerTest.php
+++ tests/DataViewRequestHandlerTest.php
@@ -13,30 +13,52 @@ class DataViewRequestHandlerTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @covers DataViewRequestHandler::bind
+     * @covers DataViewRequestHandler::clearSessionSettings
      */
-    public function test_initialPageLoad()
+    public function testBind_initialPageLoad()
     {
         $formFactory = $this->getMockBuilder('\Symfony\Component\Form\FormFactory')->disableOriginalConstructor()->getMock();
 
         $session = $this->getMockBuilder('\Symfony\Component\HttpFoundation\Session\Session')->disableOriginalConstructor()->getMock();
-        $session
-            ->expects($this->once())
-            ->method('clear')
-            ->with($this->equalTo('data_view'));
+        $session->expects($this->once())->method('clear')->with($this->equalTo('data_view'));
 
         $dataView = $this->getMockBuilder('\DataView\DataView')->disableOriginalConstructor()->getMock();
-        $dataView
-            ->expects($this->once())
-            ->method('getColumns')
-            ->will($this->returnValue(array()));
 
         $request = $this->getMockBuilder('\Symfony\Component\HttpFoundation\Request')->disableOriginalConstructor()->getMock();
-        $request
-            ->expects($this->exactly(2))
-            ->method('getMethod')
-            ->will($this->returnValue('GET'));
+        $request->expects($this->once())->method('getMethod')->will($this->returnValue('GET'));
 
-        $dataViewRequestHandler = new DataViewRequestHandler($formFactory, $session);
+        $dataViewRequestHandler = $this->getMock('\FOF\DataViewBundle\Lib\DataViewRequestHandler', 
+            array('handleFilters'), array($formFactory, $session));
+        $dataViewRequestHandler->expects($this->once())->method('handleFilters')->with($this->equalTo($dataView), $this->equalTo($request));
+
+        $dataViewRequestHandler->bind($dataView, $request);
+    }
+
+    /**
+     * @covers DataViewRequestHandler::bind
+     * @covers DataViewRequestHandler::clearSessionSettings
+     * @covers DataViewRequestHandler::saveSessionSettings
+     */
+    public function testBind_post()
+    {
+        $formFactory = $this->getMockBuilder('\Symfony\Component\Form\FormFactory')->disableOriginalConstructor()->getMock();
+
+        $session = $this->getMockBuilder('\Symfony\Component\HttpFoundation\Session\Session')->disableOriginalConstructor()->getMock();
+        $session->expects($this->once())->method('get')->with($this->equalTo('data_view'))->will($this->returnValue(array('page' => 2)));
+        $session->expects($this->once())->method('set')->with($this->equalTo('data_view'), $this->equalTo(array('page' => 3)));
+
+        $dataView = $this->getMockBuilder('\DataView\DataView')->disableOriginalConstructor()->getMock();
+        $dataView->expects($this->once())->method('setCurrentPage')->with($this->equalTo(2));
+        $dataView->expects($this->once())->method('getCurrentPage')->will($this->returnValue(3));
+
+        $request = $this->getMockBuilder('\Symfony\Component\HttpFoundation\Request')->disableOriginalConstructor()->getMock();
+
+        $dataViewRequestHandler = $this->getMock('\FOF\DataViewBundle\Lib\DataViewRequestHandler', 
+            array('handleSort', 'handlePagination', 'handleFilters'), array($formFactory, $session));
+        $dataViewRequestHandler->expects($this->once())->method('handleSort')->with($this->equalTo($dataView), $this->equalTo($request));
+        $dataViewRequestHandler->expects($this->once())->method('handlePagination')->with($this->equalTo($dataView), $this->equalTo($request));
+        $dataViewRequestHandler->expects($this->once())->method('handleFilters')->with($this->equalTo($dataView), $this->equalTo($request));
+
         $dataViewRequestHandler->bind($dataView, $request);
     }
 }
